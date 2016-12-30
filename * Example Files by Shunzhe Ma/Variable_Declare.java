466cf38d1d438324943c1c5128a2c0eb85220ddf
Shunzhe Ma <shunzhema@users.noreply.github.com>
2016-12-31 00:03:23 +0800

    Update Variable_Declare.java

--- * Example Files by Shunzhe Ma/Variable_Declare.java
+++ * Example Files by Shunzhe Ma/Variable_Declare.java
@@ -1,22 +1,21 @@
-
 public class DeclareAVariable {
 
 	public static void main(String[] args) 
 	{
 	// TODO Auto-generated method stub
-        String name = Frances;
-	String gender = Female;
+        String name = "Frances";
+	String gender = "Female";
         int yearComing = 2017;
-	String[] feeling = {"Cool", " Exciting", " Fantastic", " and Lovely"}
+	String[] feeling = {"Cool", " Exciting", " Fantastic", " and Lovely"};
 	//String parnter = "Sunny";
 	//Int is used to declear a number variable
 	
         String combination = gender + name + "wants a" ;
-	for(int item:feeling)
+	for(String item:feeling)
 	{
 		combination+=item;
 	}
-        combination+= yearComing.
+        combination+= yearComing;
 		
 	System.out.print(combination);
 		
