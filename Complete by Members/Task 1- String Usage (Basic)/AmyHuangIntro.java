721d5d3891da83f19f116cb969a6b576490b3e0a
Amy <1179140002@qq.com>
2016-12-31 13:39:15 +0800

    introducing myself Amy
    handsome

---
+++ Complete by Members/Task 1- String Usage (Basic)/AmyHuangIntro.java
@@ -0,0 +1,24 @@
+public class DeclareAVariable {
+
+	public static void main(String[] args) 
+	{
+	// TODO Auto-generated method stub
+        String name = " Amy ";
+	String like = "Handsome";
+        int yearComing = 2017;
+	String[] feeling = {"enthusiastic", " Exciting", " perfect", " and Lazy "};
+	//String parnter = "Amy";
+	//Int is used to declear a number variable
+	
+        String combination = like + name + "wants a " ;
+	for(String item:feeling)
+	{
+		combination+=item;
+	}
+        combination+= yearComing;
+		
+	System.out.print(combination);
+		
+	}
+
+}
