e26a2f2f855c716cd4edc1dd858fbbffa45e28af
Shunzhe Ma <shunzhema@users.noreply.github.com>
2017-02-25 12:43:20 +0800

    Create HelloWorld.java

---
+++ * Example Files by Shunzhe Ma/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+	// TODO Auto-generated method stub
+  String one = "Hello";
+	String two = "World!";
+	System.out.print(one + " " + two);
+	}
+
+}
